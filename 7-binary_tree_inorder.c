bb66576332bec44026ddcb137ee1d57e9fab1b5b
Xframex <loukili.issmael@gmail.com>
2023-11-02 11:07:04 -0700

    in order algo traversial

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - implementation of in order algo
+ * binary_tree_inorder - implementation of in-order traversal algo
  * @tree: tree struct
- * @func: func of node
+ * @func: node func
  * Return: Nothing
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
@@ -14,8 +14,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	}
 	else
 	{
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 	}
 }
