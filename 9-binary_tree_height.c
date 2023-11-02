680c54f538b2d67db5d3779a49575da5fa3ffd2d
Xframex <loukili.issmael@gmail.com>
2023-11-02 11:49:20 -0700

    heightof tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,18 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Calculate the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- * Return: The height of the tree.
+ * binary_tree_height - height of tree by using recursive appproch
+ * @tree: tree to go through
+ * Return: the height
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t Left = 0;
+	size_t Right = 0;
+
 	if (tree == NULL)
+	{
 		return (0);
-
-	size_t left_h = binary_tree_height(tree->left);
-	size_t right_h = binary_tree_height(tree->right);
-
-	return (left_h > right_h ? left_h + 1 : right_h + 1);
+	}
+	else
+	{
+		if (tree)
+		{
+			Left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			Right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
 
