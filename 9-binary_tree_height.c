80cdaedbf47a0367b07f15a607acc598da3b8e87
Xframex <loukili.issmael@gmail.com>
2023-11-02 11:52:39 -0700

    tree of life

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,7 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			Left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			Right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((l > r) ? l : r);
+		return ((Left > Right) ? Left : Right);
 	}
 }
 
